9fdfdc7cd766a63ba1970139fef712f01b03c914
rohan77988 <92290130+rohan77988@users.noreply.github.com>
2024-01-17 14:58:20 +0530

    Update config-exports.js

--- processors/GetIntradayPrices/config-exports.js
+++ processors/GetIntradayPrices/config-exports.js
@@ -1,10 +1,10 @@
 const config = {
-    cognito_pool_id: "eu-west-1_DMhZ3rrXS",
-    cognito_backend_client_id: "pq3rcra4kend08leir0lbth06",
-    cognito_backend_access_key: "stockMonitoring/local/backend",
-    appsync_endpoint_url: "https://efrgcmtrnfcn7oshfxf2kvsx4y.appsync-api.eu-west-1.amazonaws.com/graphql",
-    appsync_region: "eu-west-1",
-    data_feed_key: "stockMonitoring/local/datafeed",
+    cognito_pool_id: "ap-south-1_5kz2p18G8",
+    cognito_backend_client_id: "26h19emdes6f89kmsuujt7dtgm",
+    cognito_backend_access_key: "stockMonitoring/local1/backend",
+    appsync_endpoint_url: "https://ncqdpfwgsfefdecqff4w3obtfm.appsync-api.ap-south-1.amazonaws.com/graphql",
+    appsync_region: "ap-south-1",
+    data_feed_key: "stockMonitoring/local1/datafeed",
     feed_interval: "30min"
 }
 
